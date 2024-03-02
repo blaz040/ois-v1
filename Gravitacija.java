5def27ee30f14c5a64e684a490053d6e9aa965ba
AmperMeter35mA <aleksander.kozar04@gmail.com>
2024-03-02 21:31:37 +0100

    B

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,4 +2,10 @@ public class Gravitacija {
 	public static void main(String[] agrgs){
 		System.out.println("OIS, kinda mid");
 	}
+    
+    //B
+    private double izracunaj(int visina)
+    {
+        return ((Math.pow(6.674, -11) * Math.pow(5.972, 24)) / Math.pow(Math.pow(6.371, 6) + visina, 2));
+    }
 }
