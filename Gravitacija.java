8e1fb8af8b01effdadb6d9a57aef91f26072c021
blaz040 <bp71290@student.uni-lj.si>
2024-03-02 23:36:16 +0100

    testiram

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,6 +2,7 @@ public class Gravitacija {
 	public static void main(String[] agrgs){
 		System.out.println("OIS, kinda mid");
 	}
+	//sdsd
 ///asdadadasdasdasd
 	public static void izpis (double visina){
 	System.out.printf("Gravitacijski pispsek na %.2f km nadmorske visine je %.2f m/s\n", visina, izracunGravitacije(visina * 1000));
