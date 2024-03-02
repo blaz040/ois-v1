2e42e914491435edcf0db4555175de24f77cfeb3
JavaTerminator20 <mm06682@student.uni-lj.si>
2024-03-02 21:56:55 +0100

    Pripravljena funkcija za izpis

--- Gravitacija.java
+++ Gravitacija.java
@@ -2,4 +2,9 @@ public class Gravitacija {
 	public static void main(String[] agrgs){
 		System.out.println("OIS, kinda mid");
 	}
+
+	public static void izpis (double visina){
+	System.out.printf("Gravitacijski pispsek na %.2f km nadmorske visine je %.2f m/s\n", visina, izracunGravitacije(visina * 1000));
+	
+	}
 }
